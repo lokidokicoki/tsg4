1fe34baafcca855d16de8b6b05e7962d5c14f3f9
Robin Tregaskis <lokidokicoki@gmail.com>
2023-07-02 20:05:55 +0100

    feat(main): Stuff live, spawn and die

--- main.py
+++ main.py
@@ -2,87 +2,223 @@
 TSG4
 """
 import random
-from typing import List, Tuple
+from typing import List, Optional, Tuple
 
 import pygame as pg
 
 TITLE = "TSG4"
-UPDATE_PERIOD = 1  # number of seconds between updates
+UPDATE_PERIOD = 0.01  # number of seconds between updates
+TICK_SPEED = 1000
 resolution = (800, 800)
-world_size = (4, 4)  # rows, cols)
+WORLD_WIDTH = 40
+WORLD_HEIGHT = 40
+STUFF_CHANCE = 10
 
 counters = {"T": 1, "S": 1, "G": 1}
 
 
-def evaluate_dims():
-    cell_width = resolution[0] / world_size[0]
-    cell_height = resolution[1] / world_size[1]
+def evaluate_dims() -> Tuple[float, float]:
+    """
+    Get base cell width and height base on resolution and world dimensions
+    """
+    cell_width = resolution[0] / WORLD_WIDTH
+    cell_height = resolution[1] / WORLD_HEIGHT
 
     return (cell_width, cell_height)
 
 
 class BaseTSG:
-    def __init__(self, prefix: str):
+    """
+    Base class for TSG classes.
+    """
+
+    def __init__(self, surface: pg.Surface, prefix: str, row: int, col: int, color: pg.Color):
+        self.surface = surface
         self.energy = 1
+        self.age = 0
+        self.lifespan = 0
+        self.spawn_threshold = 0
+        self.row = row
+        self.col = col
+        self.color = color
+        self.dead = False  # is this instnace alive or dead
         self.name = f"{prefix}{counters[prefix]}"
         counters[prefix] += 1
 
+    def draw(self):
+        pass
+
+    def process(self, do_actions: bool):
+        pass
+
 
 class Stuff(BaseTSG):
-    def __init__(self, surface, row: int, col: int, size: int):
-        super().__init__("S")
-        self.surface = surface
-        self.color = pg.Color(0, 10, 0)
+    """
+    Stuff - this grows on the substrate of the World.
+    It gains energy at a set rate, and will spawn new stuff in empty spaces next to it
+    """
+
+    def __init__(self, manager, surface, row: int, col: int, size: int):
+        super().__init__(surface, "S", row, col, pg.Color(0, 10, 0))
+        self.manager = manager
         self.size = size / 2
+        self.lifespan = 50
+        self.spawn_threshold = 20  # amount of energy required to spawn
         self.pos = (
             (size * ((row * size) // size)) + self.size,
             (size * ((col * size) // size)) + self.size,
         )
 
     def __str__(self):
-        return f"{self.name}, pos: {self.pos}, size: {self.size}, energy: {self.energy}"
+        return f"{self.name}, cell: ({self.row},{self.col}), age: {self.age}, energy: {self.energy}"
 
-    def process(self, grow: bool):
-        if grow:
-            self.energy += 1
-            self.color.g = min(200, 10 * self.energy)
-            # print(str(self))
+    def process(self, do_actions: bool):
+        super().process(do_actions)
 
-        # spawn a new stuff
-        if self.energy > 20:
-            print("spawn")
+        if do_actions:
+            self.eat()
+            self.spawn()
+            self.die()
 
         self.draw()
+
+    def eat(self):
+        self.energy += 1
+        self.age += 1
+        self.color.g = min(200, 10 * self.energy)
+
+    def spawn(self):
+        # spawn a new stuff
+        if self.energy > self.spawn_threshold:
+            # check for free adjacent squares in manager
+            has_free_cell, row, col = self.manager.get_next_free_cell(self.row, self.col)
+
+            if has_free_cell:
+                self.manager.add_stuff(row, col)
+                self.energy = int(self.energy / 2)
+            else:
+                self.die(True)
+
+    def die(self, force: bool = False):
+        if force or self.age > self.lifespan:
+            self.dead = True
+
+        if self.dead:
+            self.color = pg.Color(255, 0, 0)
 
     def draw(self):
         pg.draw.circle(self.surface, self.color, self.pos, self.size)
 
 
-class StuffManager:
+class BaseManager:
+    """
+    Base Maanger for TSG instances
+    """
+
     def __init__(self, surface: pg.Surface, cell_dims: Tuple[int, int]):
         self.surface = surface
         self.cell_dims = cell_dims
-        self.place_chance = 3
-        self.stuffs_matrix: List[List[Stuff]] = [
-            [None for col in range(world_size[0])] for row in range(world_size[1])
+        self.matrix: List[List[Optional[BaseTSG]]] = [
+            [None for col in range(WORLD_WIDTH)] for row in range(WORLD_HEIGHT)
         ]
 
-    def place_stuff(self):
-        for row in range(world_size[0]):
-            for col in range(world_size[1]):
-                can_place = random.randint(0, 3)
-                if can_place == self.place_chance:
-                    self.stuffs_matrix[row][col] = Stuff(self.surface, row, col, self.cell_dims[0])
+    def get_next_free_cell(self, row, col) -> Tuple[bool, int, int]:
+        """
+        Find the next empty cell in the matrix starting top left and moving clockwise
+        """
+        # check adjacent cells to passed row and col
+        if (row - 1 >= 0) and (col - 1 >= 0) and self.matrix[row - 1][col - 1] is None:
+            return (True, row - 1, col - 1)
+        if (row - 1 >= 0) and self.matrix[row - 1][col] is None:
+            return (True, row - 1, col)
+        if (row - 1 >= 0) and (col + 1 < WORLD_HEIGHT) and self.matrix[row - 1][col + 1] is None:
+            return (True, row - 1, col + 1)
 
-    def process_stuff(self, grow: bool = False):
-        for row in range(world_size[0]):
-            for col in range(world_size[1]):
-                stuff = self.stuffs_matrix[row][col]
+        if (col - 1 >= 0) and self.matrix[row][col - 1] is None:
+            return (True, row, col - 1)
+        if (col + 1 < WORLD_HEIGHT) and self.matrix[row][col + 1] is None:
+            return (True, row, col + 1)
+
+        if (row + 1 < WORLD_WIDTH) and (col - 1 >= 0) and self.matrix[row + 1][col - 1] is None:
+            return (True, row + 1, col - 1)
+        if (row + 1 < WORLD_WIDTH) and self.matrix[row + 1][col] is None:
+            return (True, row + 1, col)
+        if (
+            (row + 1 < WORLD_WIDTH)
+            and (col + 1 < WORLD_HEIGHT)
+            and self.matrix[row + 1][col + 1] is None
+        ):
+            return (True, row + 1, col + 1)
+
+        return (False, row, col)
+
+    def remove(self, row: int, col: int):
+        """
+        Remove TSG instance from matrix
+        """
+        self.matrix[row][col] = None
+
+
+class StuffManager(BaseManager):
+    """
+    Manages the Stuff instances in the World.
+
+    - randomly places stuff at creation
+    - culls dead stuff
+    - pokes stuff to process their actions
+    """
+
+    def __init__(self, surface: pg.Surface, cell_dims: Tuple[int, int]):
+        super().__init__(surface, cell_dims)
+        self.place_chance = STUFF_CHANCE
+
+    def place_stuff(self):
+        """
+        Initial World with randomly placed Stuff
+        """
+        for row in range(WORLD_WIDTH):
+            for col in range(WORLD_HEIGHT):
+                can_place = random.randint(0, STUFF_CHANCE)
+                if can_place == self.place_chance:
+                    self.add_stuff(row, col)
+
+    def add_stuff(self, row: int, col: int):
+        """
+        Add a new Stuff instance to the matrix
+        :param row: which row
+        :param col: which col
+        """
+        self.matrix[row][col] = Stuff(self, self.surface, row, col, self.cell_dims[0])
+
+    def cull_stuff(self):
+        """
+        Cull Stuff marked as 'dead' from the matrix
+        """
+        for row in range(WORLD_WIDTH):
+            for col in range(WORLD_HEIGHT):
+                stuff = self.matrix[row][col]
+                if stuff and stuff.dead:
+                    self.remove(row, col)
+
+    def process_stuff(self, do_actions: bool = False):
+        """
+        Check each cell in matrix, if a Stuff is present,
+        tell it to process its actions
+        """
+        for row in range(WORLD_WIDTH):
+            for col in range(WORLD_HEIGHT):
+                stuff = self.matrix[row][col]
                 if stuff:
-                    stuff.process(grow)
+                    stuff.process(do_actions)
 
 
 class Game:
+    """
+    Manages the game
+    - draws the substrate
+    - handles TSG managers
+    """
+
     def __init__(self):
         pg.init()
         self.clock = pg.time.Clock()  # set to amx FPS
@@ -96,6 +232,10 @@ class Game:
         self.stuff_manager = StuffManager(self.surface, self.cell_dims)
 
     def main(self):
+        """
+        Main entry point and set up
+        """
+
         self.stuff_manager.place_stuff()
         while self.loop:
             self.main_loop()
@@ -103,19 +243,27 @@ class Game:
         pg.quit()
 
     def main_loop(self):
+        """
+        Main loop
+        - checks clock for next action tick
+        - renders substrate
+        - calls manager methods
+        - displays number of action ticks so far
+        - event handling
+        """
         self.clock.tick(30)
         self.surface.fill((0, 0, 0))
         # has enough time passed to perform an action?
         do_actions = False
-        now = pg.time.get_ticks() / 1000
+        now = pg.time.get_ticks() / TICK_SPEED
         if now - self.last_update > UPDATE_PERIOD:
             do_actions = True
             self.last_update = now
             self.num_ticks += 1
 
         # render alternating grid
-        for row in range(world_size[0]):
-            for col in range(row % 2, world_size[1], 2):
+        for row in range(WORLD_WIDTH):
+            for col in range(row % 2, WORLD_HEIGHT, 2):
                 rect = (
                     row * self.cell_dims[0],
                     col * self.cell_dims[1],
@@ -124,6 +272,7 @@ class Game:
                 )
                 pg.draw.rect(self.surface, (40, 40, 40), rect)
 
+        self.stuff_manager.cull_stuff()
         self.stuff_manager.process_stuff(do_actions)
 
         tick_text = f"Tick: {self.num_ticks}"
