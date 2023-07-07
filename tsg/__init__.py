f4d7ef586bcf15e7b9b68c8d3e6e5854eeb86f97
Robin Tregaskis <lokidokicoki@gmail.com>
2023-07-07 21:33:10 +0100

    chore: move TSGConfig into types

--- tsg/__init__.py
+++ tsg/__init__.py
@@ -1,9 +1,8 @@
 """
 isort: skip_file
 """
-from .types import Cell
+from .types import Cell, TSGConfig
 from .base_tsg import BaseTSG
-from .tsg_config import TSGConfig
 from .base_manager import BaseManager
 from .stuff import Stuff
 from .stuff_manager import StuffManager
--- tsg/types.py
+++ tsg/types.py
@@ -12,3 +12,21 @@ class Cell:
 
     row: int
     col: int
+
+
+@dataclass
+class TSGConfig:
+    """
+    Config details
+    """
+
+    update_period: float
+    tick_speed: int
+    resolution_w: int
+    resolution_h: int
+    world_width: int
+    world_height: int
+    stuff_chance: int
+    gack_chance: int
+    gack_size: int
+    thing_chance: int
