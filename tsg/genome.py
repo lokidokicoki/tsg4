6bee3c40194a0dd9c0b58d25564d72cceecce483
Robin Tregaskis <lokidokicoki@gmail.com>
2023-07-20 21:28:38 +0100

    feat(genome): add spin_rate, spin_cw, spin_ccw genes. add Gene.__str__

--- tsg/genome.py
+++ tsg/genome.py
@@ -20,7 +20,9 @@ class GeneType(Enum):
     SPEED = 1
     HUNGER = 2
     SPAWN = 3
-    SPIN = 4
+    SPIN_RATE = 4
+    SPIN_CW = 5
+    SPIN_CCW = 6
 
 
 class Gene:
@@ -51,6 +53,9 @@ class Gene:
     def get_random_weight() -> int:
         return random.randint(0, MAX_GENE_WEIGHT) - WEIGHT_SHIFT
 
+    def __str__(self):
+        return f"{self.gene_type}:{self.weight}:{self.get_weight_as_float()}"
+
 
 class Genome:
     """
@@ -71,7 +76,9 @@ class Genome:
         self.genes.append(Gene(GeneType.SPEED))
         self.genes.append(Gene(GeneType.HUNGER))
         self.genes.append(Gene(GeneType.SPAWN))
-        self.genes.append(Gene(GeneType.SPIN))
+        self.genes.append(Gene(GeneType.SPIN_RATE))
+        self.genes.append(Gene(GeneType.SPIN_CW))
+        self.genes.append(Gene(GeneType.SPIN_CCW))
 
     def get_gene_by_type(self, gene_type: GeneType) -> Optional[Gene]:
         return next((x for x in self.genes if x.gene_type == gene_type), None)
